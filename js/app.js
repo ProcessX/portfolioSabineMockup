bdc6167d75f2b3ff82314897730d37efb1c06f36
ProcessX <luc2526@gmail.com>
2020-12-22 13:31:27 +0100

    Work - OK

--- js/app.js
+++ js/app.js
@@ -1,9 +1,13 @@
+var work__li;
+
 $(document).ready(function(){
     console.log('Ready');
 
     $('.btn--burger').click(() => {
         toggleBurger();
     });
+
+    initWorkList();
 });
 
 
@@ -11,4 +15,15 @@ toggleBurger = () => {
     console.log('Open Burger');
     $('.btn--burger').toggleClass('btn--burger--open');
     $('.navbar').toggleClass('navbar--open');
+}
+
+
+initWorkList = () => {
+    work__li = $('.work__el');
+
+    console.log(work__li);
+
+    for(let i = 0; i < work__li.length; i++){
+        console.log(work__li[i]);
+    }
 }
