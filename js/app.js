fdbe979f97ee3a24b4eacd14097bf94da1eb993b
ProcessX <luc2526@gmail.com>
2020-12-26 15:46:31 +0100

    Work img transition : done

--- js/app.js
+++ js/app.js
@@ -7,7 +7,6 @@ $(document).ready(function(){
         toggleBurger();
     });
 
-    initWorkList();
 });
 
 
@@ -15,15 +14,4 @@ toggleBurger = () => {
     console.log('Open Burger');
     $('.btn--burger').toggleClass('btn--burger--open');
     $('.navbar').toggleClass('navbar--open');
-}
-
-
-initWorkList = () => {
-    work__li = $('.work__el');
-
-    console.log(work__li);
-
-    for(let i = 0; i < work__li.length; i++){
-        console.log(work__li[i]);
-    }
 }
